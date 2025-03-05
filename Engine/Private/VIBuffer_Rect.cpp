b9dff371e67add88025bbb8822f21ffd68ad21bc
Bamtoliya <Bamtoliya@gmail.com>
2025-03-05 15:25:47 +0900

    지형 버퍼 추가

--- Engine/Private/VIBuffer_Rect.cpp
+++ Engine/Private/VIBuffer_Rect.cpp
@@ -13,11 +13,16 @@ CVIBuffer_Rect::CVIBuffer_Rect(const CVIBuffer& Prototype)
 HRESULT CVIBuffer_Rect::Initialize_Prototype()
 {
 
-#pragma region VERTEX_BUFFER
-	m_iNumVertices = 6;
+	m_iNumVertices = 4;
 	m_iVertexStride = sizeof(VTXPOSTEX);
 	m_iFVF = D3DFVF_XYZ | D3DFVF_TEX1/* | D3DFVF_TEXCOORDSIZE2(0)*/;
+	m_iNumPritimive = 2;
 
+	m_iIndexStride = 2;
+	m_iNumIndices = 6;
+	m_eIndexFormat = D3DFMT_INDEX16;
+
+#pragma region VERTEX_BUFFER
 	if (FAILED(__super::Create_VertexBuffer()))
 		return E_FAIL;
 
@@ -33,18 +38,31 @@ HRESULT CVIBuffer_Rect::Initialize_Prototype()
 	pVertices[1].vTexcoord = _float2(1.f, 0.f);
 
 	pVertices[2].vPosition = _float3(0.5f, -0.5f, 0.f);
-	pVertices[2].vTexcoord = _float2(1.f, 1.f);
+	pVertices[2].vTexcoord = _float2(1.f, 1.f);		
 
-	pVertices[3].vPosition = _float3(-0.5f, 0.5f, 0.f);
-	pVertices[3].vTexcoord = _float2(0.f, 0.f);
-
-	pVertices[4].vPosition = _float3(0.5f, -0.5f, 0.f);
-	pVertices[4].vTexcoord = _float2(1.f, 1.f);
-
-	pVertices[5].vPosition = _float3(-0.5f, -0.5f, 0.f);
-	pVertices[5].vTexcoord = _float2(0.f, 1.f);
+	pVertices[3].vPosition = _float3(-0.5f, -0.5f, 0.f);
+	pVertices[3].vTexcoord = _float2(0.f, 1.f);
 
 	m_pVB->Unlock();
+#pragma endregion
+
+#pragma region INDEX_BUFFER
+	if (FAILED(__super::Create_IndexBuffer()))
+		return E_FAIL;
+
+	_ushort* pIndices = { nullptr };
+
+	m_pIB->Lock(0, 0, reinterpret_cast<void**>(&pIndices), 0);
+
+	pIndices[0] = 0;
+	pIndices[1] = 1;
+	pIndices[2] = 2;
+
+	pIndices[3] = 0;
+	pIndices[4] = 2;
+	pIndices[5] = 3;
+
+	m_pIB->Unlock();
 #pragma endregion
 
 	return S_OK;
--- Engine/Public/VIBuffer.h
+++ Engine/Public/VIBuffer.h
@@ -19,6 +19,9 @@ public:
 	virtual HRESULT Initialize(void* pArg) override;
 	virtual HRESULT Render();
 
+public:
+	HRESULT Bind_Buffers();
+
 protected:
 	/*  정점 배열을 가지고 있는 컴 객체. */
 	LPDIRECT3DVERTEXBUFFER9				m_pVB = { nullptr };
@@ -27,9 +30,15 @@ protected:
 	_uint								m_iNumVertices = { };
 	_uint								m_iVertexStride = { };
 	_uint								m_iFVF = {};
+	_uint								m_iNumPritimive = {};
+
+	_uint								m_iNumIndices = {};
+	_uint								m_iIndexStride = {};
+	D3DFORMAT							m_eIndexFormat = {};
 
 protected:
 	HRESULT Create_VertexBuffer();
+	HRESULT Create_IndexBuffer();
 
 public:
 	virtual CComponent* Clone(void* pArg) = 0;
