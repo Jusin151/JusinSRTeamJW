7faddaa309f64900482f252893a0851a3329e830
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2025-03-12 21:06:18 +0900

    사운드 디바이스 기초(?) 추가

--- Engine/Public/GameInstance.h
+++ Engine/Public/GameInstance.h
@@ -47,13 +47,17 @@ public:
 	void Update_Timer(const _wstring& strTimerTag);
 #pragma endregion
 
+#pragma region SOUND_MANAGER
+
 private:
 	class CGraphic_Device*		m_pGraphic_Device = { nullptr };
+	class CSound_Device*		m_pSound_Device = { nullptr };
 	class CLevel_Manager*		m_pLevel_Manager = { nullptr };
 	class CPrototype_Manager*	m_pPrototype_Manager = { nullptr };
 	class CObject_Manager*		m_pObject_Manager = { nullptr };
 	class CRenderer*			m_pRenderer = { nullptr };
 	class CTimer_Manager*		m_pTimer_Manager = { nullptr };
+	
 
 public:
 	void Release_Engine();
