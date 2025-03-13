303969981b7e81e7be13b4b49214a95d147242d3
Bamtoliya <Bamtoliya@gmail.com>
2025-03-13 16:51:42 +0900

    업데이트 FMODLIB

---
+++ UpdateFMODLib.bat
@@ -0,0 +1,3 @@
+xcopy /y ".\External Libraries\fmod\core\lib\x64\*.dll"			.\Client\bin\
+xcopy /y ".\External Libraries\fmod\fsbank\lib\x64\*.dll"			.\Client\bin\
+xcopy /y ".\External Libraries\fmod\studio\lib\x64\*.dll"			.\Client\bin\
