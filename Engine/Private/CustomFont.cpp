ae8df65c40ac2ef4c7ce461b51bc04e142f840bc
cowkjw <cowkjw@naver.com>
2025-03-15 15:10:25 +0900

    폰트 매니저 추가

---
+++ Engine/Private/CustomFont.cpp
@@ -0,0 +1,92 @@
+#include "..\Public\CustomFont.h"
+
+CCustomFont::CCustomFont(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: m_pGraphic_Device{ pGraphic_Device }
+{
+	Safe_AddRef(m_pGraphic_Device);
+	ZeroMemory(&m_FontDesc, sizeof(D3DXFONT_DESC));
+}
+
+HRESULT CCustomFont::Initialize(const wstring& strFontFilePath)
+{
+	// 폰트 파일 경로에서 폰트 이름 추출 또는 기본 폰트 사용
+	// DX9에서는 TTF 파일을 직접 로드하는 대신 시스템 폰트를 사용
+
+	m_FontDesc.Height = 16;                  // 폰트 높이
+	m_FontDesc.Width = 0;                    // 폰트 너비 (0은 자동)
+	m_FontDesc.Weight = FW_NORMAL;           // 폰트 두께
+	m_FontDesc.MipLevels = 1;
+	m_FontDesc.Italic = FALSE;
+	m_FontDesc.CharSet = DEFAULT_CHARSET;
+	m_FontDesc.OutputPrecision = OUT_DEFAULT_PRECIS;
+	m_FontDesc.Quality = DEFAULT_QUALITY;
+	m_FontDesc.PitchAndFamily = DEFAULT_PITCH | FF_DONTCARE;
+	wcscpy_s(m_FontDesc.FaceName, L"굴림");  // 기본 폰트 이름
+
+	// 폰트 파일 이름이 지정된 경우 사용
+	if (!strFontFilePath.empty())
+	{
+		// 간단한 예: 파일 이름에서 .ttf 확장자 제거
+		wstring strFontName = strFontFilePath;
+		size_t pos = strFontName.rfind(L'\\');
+		if (pos != wstring::npos)
+			strFontName = strFontName.substr(pos + 1);
+
+		pos = strFontName.rfind(L'.');
+		if (pos != wstring::npos)
+			strFontName = strFontName.substr(0, pos);
+
+		if (!strFontName.empty() && strFontName.length() < LF_FACESIZE)
+			wcscpy_s(m_FontDesc.FaceName, strFontName.c_str());
+	}
+
+	// D3DX 폰트 생성
+	if (FAILED(D3DXCreateFontIndirect(m_pGraphic_Device, &m_FontDesc, &m_pFont)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CCustomFont::Render(const wstring& strText, const _float2& vPosition, _float3 vColor)
+{
+	if (nullptr == m_pFont)
+		return E_FAIL;
+
+	// 텍스트 렌더링 영역 설정
+	RECT rc;
+	SetRect(&rc, (int)vPosition.x, (int)vPosition.y, 0, 0);
+
+	// D3DCOLOR로 색상 변환
+	D3DCOLOR fontColor = D3DCOLOR_XRGB(
+		(int)(vColor.x * 255.0f),
+		(int)(vColor.y * 255.0f),
+		(int)(vColor.z * 255.0f));
+
+	// 텍스트 그리기
+	m_pFont->DrawTextW(
+		NULL,
+		strText.c_str(),
+		-1,
+		&rc,
+		DT_NOCLIP,
+		fontColor);
+
+	return S_OK;
+}
+
+CCustomFont* CCustomFont::Create(LPDIRECT3DDEVICE9 pGraphic_Device, const wstring& strFontFilePath)
+{
+	CCustomFont* pInstance = new CCustomFont(pGraphic_Device);
+	if (FAILED(pInstance->Initialize(strFontFilePath)))
+	{
+		MSG_BOX("Failed to Created : CCustomFont");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CCustomFont::Free()
+{
+	Safe_Release(m_pFont);
+	Safe_Release(m_pGraphic_Device);
+}
---
+++ Engine/Private/Font_Manager.cpp
@@ -0,0 +1,57 @@
+#include "Font_Manager.h"
+#include "CustomFont.h"
+
+CFont_Manager::CFont_Manager(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: m_pGraphic_Device{ pGraphic_Device }
+{
+	Safe_AddRef(m_pGraphic_Device);
+}
+
+HRESULT CFont_Manager::Add_Font(const _wstring& strFontTag, const _wstring& strFontFilePath)
+{
+	if (nullptr != Find_Font(strFontTag))
+		return E_FAIL;
+
+	CCustomFont* pFont = CCustomFont::Create(m_pGraphic_Device, strFontFilePath);
+	if (nullptr == pFont)
+		return E_FAIL;
+
+	m_Fonts.emplace(strFontTag, pFont);
+
+	return S_OK;
+}
+
+HRESULT CFont_Manager::Render_Font(const _wstring& strFontTag, const _wstring& strText, const _float2& vPosition, _float3 vColor)
+{
+	CCustomFont*		pFont = Find_Font(strFontTag);
+	if (nullptr == pFont)
+		return E_FAIL;
+
+	return pFont->Render(strText, vPosition, vColor);
+}
+
+CCustomFont * CFont_Manager::Find_Font(const wstring & strFontTag)
+{
+	auto	iter = m_Fonts.find(strFontTag);
+
+	if(iter == m_Fonts.end())		
+		return nullptr;
+
+	return iter->second;
+}
+
+CFont_Manager * CFont_Manager::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+{
+	return new CFont_Manager(pGraphic_Device);
+}
+
+void CFont_Manager::Free()
+{
+	for (auto& Pair : m_Fonts)
+		Safe_Release(Pair.second);
+
+	m_Fonts.clear();
+
+	Safe_Release(m_pGraphic_Device);
+}
+
