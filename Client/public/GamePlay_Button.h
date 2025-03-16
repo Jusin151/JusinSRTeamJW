0f128cdff0d30db8d6885dca61760d7cc3f1c0eb
DongWan-Hong <thsu5957@naver.com>
2025-03-16 15:52:23 +0900

    구조 개선 및 포인트 상점 UI추가
    게임플레이 버튼 구조 개선
    포인트 상점 UI 각 칸별로 피킹에 따른
    이미지 적용  , 왼쪽 버튼 작업중

--- Client/public/GamePlay_Button.h
+++ Client/public/GamePlay_Button.h
@@ -16,40 +16,8 @@ BEGIN(Client)
 class CGamePlay_Button : public CUI_Base
 {
 public:
-    enum Button_type // 이 버튼은 어느 메뉴창의 버튼인지
-    {
-        Episode = 0, Point_Shop = 1, Spell_Shop = 2, Upgrade_Weapon = 3
-    };
-    enum Level_01_Type // 이 버튼은 어느 이미지를 사용할것인지
-    {
-        Level_ICon_Defaul, Level_ICon_Selected,
-        Stage_01_Color_Default, Stage_01_Color_Selected,
-        Stage_02_Gray_Default, Stage_02_Gray_Selected, Stage_02_Color_Default, Stage_02_Color_Selected,
-        Stage_03_Gray_Default, Stage_03_Gray_Selected, Stage_03_Color_Default, Stage_03_Color_Selected,
-        Stage_04_Gray_Default, Stage_04_Gray_Selected, Stage_04_Color_Default, Stage_04_Color_Selected,
-        Stage_05_Gray_Default, Stage_05_Gray_Selected, Stage_05_Color_Default, Stage_05_Color_Selected,
-        Episode_Display=20,
-
-        Point_Shop_Display=0,Point_Shop_Selected=1
-    };
-    typedef struct tagEpisode //에피소드 버튼 타입
-    {
-        _bool bLevel_Icon_Button_Flag{};
-        _bool bLevel_01_Stage_Button_Flag{};
-        _bool bLevel_02_Stage_Button_Flag{};
-        _bool bLevel_03_Stage_Button_Flag{};
-        _bool bLevel_04_Stage_Button_Flag{};
-        _bool bLevel_05_Stage_Button_Flag{};
-    }Episode_Button_Type_Desc;
-    typedef struct tagPoint_Shop //에피소드 버튼 타입
-    {
-        _bool bLevel_Icon_Button_Flag{};
-        _bool bLevel_01_Stage_Button_Flag{};
-        _bool bLevel_02_Stage_Button_Flag{};
-        _bool bLevel_03_Stage_Button_Flag{};
-        _bool bLevel_04_Stage_Button_Flag{};
-        _bool bLevel_05_Stage_Button_Flag{};
-    }Point_Shop_Button_Type_Desc;
+  
+ 
 
     typedef struct tagButton
     {
@@ -60,7 +28,7 @@ public:
         Button_type Button_type{}; // 이 버튼이 4종류의 상점중 어느 상점의 버튼인지
         Episode_Button_Type_Desc Episode_Button_Type{}; // 이 버튼의 에피소드 버튼 타입
         _bool bDisplay_On{};
-        _bool Point_Shop_Seleted[12];
+        _bool Point_Shop_Seleted[16]{};
     }GamePlayer_Button_Desc;
  
 
