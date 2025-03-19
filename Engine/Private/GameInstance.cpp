fd66d73206de322adde235f0470e3fa778b9e831
Bamtoliya <Bamtoliya@gmail.com>
2025-03-19 15:31:43 +0900

    빌드, 실행 확인

--- Engine/Private/GameInstance.cpp
+++ Engine/Private/GameInstance.cpp
@@ -214,7 +214,8 @@ void CGameInstance::Update_Timer(const _wstring& strTimerTag)
 #pragma region PICKING
 _bool CGameInstance::Picking(_float3& vPickedPos, const _float3& vPointA, const _float3& vPointB, const _float3& vPointC)
 {
-	return m_pPicking->Picking(vPickedPos, vPointA, vPointB, vPointC);
+	return false;
+	//return m_pPicking->Picking(vPickedPos, vPointA, vPointB, vPointC);
 }
 
 #pragma endregion
