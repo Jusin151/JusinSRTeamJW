308fe7d3a0590166010c2c02fb51003c62967c3c
DongWan-Hong <thsu5957@naver.com>
2025-04-06 19:16:30 +0900

    보스 수정

--- Client/private/HellBoss_AttackState.cpp
+++ Client/private/HellBoss_AttackState.cpp
@@ -4,51 +4,33 @@
 #include "HellBoss_Bullet.h"
 #include "GameInstance.h" 
 
-
 void CHellBoss_AttackState::Enter(CHellBoss* pBoss)
 {
     // 공격 애니메이션 설정
     pBoss->Set_Animation("3_EyeBlast");
-   
+
+     
         if (FAILED(pBoss->Get_GameInstance()->Add_GameObject(LEVEL_HONG, TEXT("Prototype_GameObject_HellBoss_Bullet"),
             LEVEL_HONG, TEXT("Layer_HellBoss_Bullet"))))
         {
             MSG_BOX("HellBoss_Bullet 생성 실패");
         }
-
 }
-
-#include "Pattern_EyeBlast.h"
-#include "HellBoss_Bullet.h" // 필요 시
-
 void CHellBoss_AttackState::Update(CHellBoss* pBoss, float fDeltaTime)
 {
-    // EyeBlast일 때만 총알 발사
-    if (dynamic_cast<CPattern_EyeBlast*>(pBoss->Get_AttackPattern()))
-    {
-        static bool bHasFired = false;
 
-        if (!bHasFired && pBoss->Get_CurAnimationFrame() >= 15)
-        {
-            bHasFired = true;
+	pBoss->Use_Attack(fDeltaTime);
 
-            if (FAILED(pBoss->Get_GameInstance()->Add_GameObject(
-                LEVEL_HONG,
-                TEXT("Prototype_GameObject_HellBoss_Bullet"),
-                LEVEL_HONG,
-                TEXT("Layer_HellBoss_Bullet"))))
-            {
-                MSG_BOX("HellBoss_Bullet 생성 실패");
-            }
-        }
+	auto pPattern = pBoss->Get_AttackPattern();
+	if (pPattern && pPattern->Is_Finished())
+	{
+	
+		delete pPattern;
+		pBoss->Set_AttackPattern(nullptr);
 
-        if (pBoss->Get_AnimationFinished())
-            bHasFired = false; // 애니메이션 종료 후 초기화
-    }
-
-    pBoss->Use_Attack(fDeltaTime);
+		pBoss->Change_State(new CHellBoss_IdleState()); 
+	}
 }
-
 
 void CHellBoss_AttackState::Exit(CHellBoss* pBoss)
 {
