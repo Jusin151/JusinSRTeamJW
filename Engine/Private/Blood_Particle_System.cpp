2d79c5aa43c4f7cf0c634dc1de5b532b39ca31ed
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2025-04-10 17:24:59 +0900

    애니메이션변경

--- Engine/Private/Blood_Particle_System.cpp
+++ Engine/Private/Blood_Particle_System.cpp
@@ -41,7 +41,7 @@ void CBlood_Particle_System::Reset_Particle(ATTRIBUTE* pAttribute)
 	pAttribute->bIsAlive = true;
 	GetRandomVector(&pAttribute->vPosition, &m_Bound.m_vCenter, m_Bound.m_fRadius);
 	pAttribute->vPosition.z = -1.f;
-	pAttribute->vVelocity = { GetRandomFloat(-1.f, 1.0f), GetRandomFloat(-1.f, 1.0f), 0.f };
+	pAttribute->vVelocity = { GetRandomFloat(-1.f, 1.0f), GetRandomFloat(0.f, 5.0f), 0.f };
 	pAttribute->vAcceleration = { 1.5f, 1.2f, 0.0f };
 	pAttribute->fAge = 0;
 	pAttribute->fLifetime = 2.0f;
@@ -58,6 +58,7 @@ void CBlood_Particle_System::Update(float fTimeDelta)
 		if (i.bIsAlive)
 		{
 			i.vPosition += (i.vVelocity * i.vAcceleration.x) * fTimeDelta;
+			i.vVelocity.y -= GRAVITY * fTimeDelta;
 			i.fAge += fTimeDelta;
 			i.fSize = m_fSize / D3DXVec3Length(&i.vPosition);
 			if (i.fAge > i.fLifetime)
