2c6d6c6b5a5c4cf4d7644ab2dbca8f3df602c30c
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2025-04-16 22:02:01 +0900

    탄피 높이 조정

--- Engine/Private/BulletShell_Particle_System.cpp
+++ Engine/Private/BulletShell_Particle_System.cpp
@@ -66,7 +66,8 @@ void CBulletShell_Particle_System::Update(float fTimeDelta)
         {
             _float3 worldPos = {};
             D3DXVec3TransformCoord(&worldPos, &i.vPosition, &m_WorldMat);
-            if (worldPos.y > m_Bounding_Box.m_vMin.y - 0.75f)
+            float floorLevelY = m_Bounding_Box.m_vMin.y - 0.6f;
+            if (worldPos.y > floorLevelY)
             {
                 i.vPosition += (i.vVelocity * i.vAcceleration.x) * fTimeDelta;
                 i.vVelocity.y -= GRAVITY * fTimeDelta;
